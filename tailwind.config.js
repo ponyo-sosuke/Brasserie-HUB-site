6c4f5785db3d69dd73f0db63efdc1e2b110ca8f1
julia <juliabadita@live.fr>
2022-05-12 18:03:44 +0200

    trying to add Doe's pages + adding cards sliders for the product page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/flowbite/**/*.js'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{html,js}',
+    './node_modules/tw-elements/dist/js/**/*.js',
+  ],
   theme: {
     screens: {
       sm: '480px',
@@ -10,9 +14,9 @@ module.exports = {
     colors: {
       'yellow-hub': '#fcd34d',
       'yellow-hub-over': '#fbbf24',
-      'blue-hub': '',
+      'blue-hub': '#10B399',
       'blue-hub-over': '',
-      'gray-hub': '',
+      'gray-hub': '#454545',
       'pink-hub': '',
       'purple-hub': '',
       blue: '#1fb6ff',
@@ -30,6 +34,49 @@ module.exports = {
       goldman: ['Goldman', 'sans-serif'],
       tourney: ['Tourney', 'sans-serif'],
     },
+    spacing: {
+      px: '1px',
+      0: '0',
+      0.5: '0.125rem',
+      1: '0.25rem',
+      1.5: '0.375rem',
+      2: '0.5rem',
+      2.5: '0.625rem',
+      3: '0.75rem',
+      3.5: '0.875rem',
+      4: '1rem',
+      5: '1.25rem',
+      6: '1.5rem',
+      7: '1.75rem',
+      8: '2rem',
+      9: '2.25rem',
+      10: '2.5rem',
+      11: '2.75rem',
+      12: '3rem',
+      14: '3.5rem',
+      16: '4rem',
+      20: '5rem',
+      24: '6rem',
+      26: '6.5rem',
+      28: '7rem',
+      30: '7.5rem',
+      32: '8rem',
+      36: '9rem',
+      40: '10rem',
+      44: '11rem',
+      48: '12rem',
+      50: '12.5rem',
+      52: '13rem',
+      56: '14rem',
+      60: '15rem',
+      64: '16rem',
+      72: '18rem',
+      80: '20rem',
+      84: '21rem',
+      88: '22rem',
+      92: '23rem',
+      96: '24rem',
+    },
   },
   extend: {
     spacing: {
@@ -41,5 +88,24 @@ module.exports = {
     },
     extend: {},
   },
-  plugins: [require('flowbite/plugin')],
+  plugins: [
+    require('tw-elements/dist/plugin'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/aspect-ratio'),
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.content-auto': {
+          'content-visibility': 'auto',
+        },
+        '.content-hidden': {
+          'content-visibility': 'hidden',
+        },
+        '.content-visible': {
+          'content-visibility': 'visible',
+        },
+      })
+    }),
+  ],
 }
